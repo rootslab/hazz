74332732114cd7f63294b2009fb5a8a23249d172
gu <44gatti@gmail.com>
2017-11-09 21:18:42 +0100

    v0.2.2 better coverage for test

--- test/hazz-test.js
+++ test/hazz-test.js
@@ -27,31 +27,36 @@ exports.test  = function ( done, assertions ) {
     log( '- max input bytes to parse (%d bytes)', max_len );
 
     log( '\n- check if size of the pseudo-random tables are correct' );
-    log( '  > table h: %d bytes', hlength );
-    log( '  > table k: %d bytes', klength );
+    log( '  > table h: %d bytes, max input length: %d', hlength, hlength >>> 11 );
+    log( '  > table k: %d bytes, max input length: %d', klength, klength >>> 11 );
     assert.ok( h.table.length === hlength, 'table size should be: ' + hlength );
     assert.ok( k.table.length === klength, 'table size should be: ' + klength );
 
-    log( '\n- check function 0 results using an Array and a String' )
+    log( '\n- check function 0 results using an Array and a String' );
     r1 = h.do( 0, s1 );
     r2 = h.do( 0, a1 );
     r3 = h.do( 0, s2 );
     r4 = h.do( 0, a2 );
 
-    assert.ok( r1 === r2, 'the result should be the same! ' + r1 + '<>' + r2 );
-    assert.ok( r2 === r3, 'the result should be the same! ' + r1 + '<>' + r2 );
-    assert.ok( r3 === r4, 'the result should be the same! ' + r1 + '<>' + r2 );   
+    assert.ok( r1 === r2, 'the result should be the same! now: ' + r1 + ' <> ' + r2 );
+    assert.ok( r2 === r3, 'the result should be the same! now: ' + r1 + ' <> ' + r2 );
+    assert.ok( r3 === r4, 'the result should be the same! now: ' + r1 + ' <> ' + r2 );   
 
-    log( '- check function 1 results using an Array and a String' )
+    log( '- check function 1 results using an Array and a String' );
+    r1 = k.do( 1, s1 );
+    r2 = k.do( 1, a1 );
+    r3 = k.do( 1, s2 );
+    r4 = k.do( 1, a2 );
+
+    assert.ok( r1 === r2, 'the result should be the same! now: ' + r1 + ' <> ' + r2 );
+    assert.ok( r2 === r3, 'the result should be the same! now: ' + r1 + ' <> ' + r2 );
+    assert.ok( r3 === r4, 'the result should be the same! now: ' + r1 + ' <> ' + r2 );   
+
+    log( '- check function 1 results after refill, reuslts should be different' );
     r1 = h.do( 1, s1 );
-    r2 = h.do( 1, a1 );
-    r3 = h.do( 1, s2 );
-    r4 = h.do( 1, a2 );
-
-    assert.ok( r1 === r2, 'the result should be the same! ' + r1 + '<>' + r2 );
-    assert.ok( r2 === r3, 'the result should be the same! ' + r1 + '<>' + r2 );
-    assert.ok( r3 === r4, 'the result should be the same! ' + r1 + '<>' + r2 );   
-
+    h.refill();
+    r2 = h.do( 1, s1 );
+    assert.ok( r1 !== r2, 'the result should be different! ' + r1 + ' === ' + r2 );
     exit();
 };
 
